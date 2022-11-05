52bae4116b4a5165b06c389c1451cefcbffd052a
Vidit Makwana <viditmakwana@Vidits-MacBook-Pro-2.local>
2022-11-05 19:04:45 -0400

    adjusted shopping list

--- js/shoppinglist.js
+++ js/shoppinglist.js
@@ -1,32 +1,5 @@
 //shopping list code
 
-    var toDolist = document.getElementsByTagName("LI");
-      var i;
-      for (i = 0; i < toDolist.length; i++) {
-          var span = document.createElement("SPAN");
-          var txt = document.createTextNode("\u00D7");
-          span.className = "close";
-          span.appendChild(txt);
-          toDolist[i].appendChild(span);
-      }
-  
-      var close = document.getElementsByClassName("close");
-      var i;
-      for (i = 0; i < close.length; i++) {
-          close[i].onclick = function() {
-           var div = this.parentElement;
-              div.style.display = "none";
-          }   
-      }
-  
-      var list = document.querySelector('ul');
-      console.log(list);
-      list.addEventListener('click', function(ev) {
-          if (ev.target.tagName === 'LI') {
-              ev.target.classList.toggle('checked');
-          }
-      }, false);
-
   
   function addShoppingItem(){
       var li = document.createElement("li");
