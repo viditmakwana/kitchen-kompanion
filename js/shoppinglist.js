3fd1a9e8bde013b834e9db605676e2fdfa2d414f
Vidit Makwana <viditmakwana@Vidits-MacBook-Pro-2.local>
2022-11-06 18:01:47 -0500

    add keyboard to shopping list

--- js/shoppinglist.js
+++ js/shoppinglist.js
@@ -5,6 +5,7 @@
       var li = document.createElement("li");
       var inputValue = document.getElementById("shoppinglistinput").value;
       var t = document.createTextNode(inputValue);
+      hideKeyboard();
       li.appendChild(t);
       document.getElementById("ul").appendChild(li);
       document.getElementById("shoppinglistinput").value = "";
@@ -23,4 +24,16 @@
           div.style.display = "none";
           }   
       }
+    }
+
+    function showKeyboard() {
+      var keyboard = document.getElementById("keyboard");
+      keyboard.setAttribute("style", "top: 122px; position: relative; opacity: 1; visibility: visible;");
+      document.getElementById("buttons").setAttribute("style", "opacity: 0; visibility: hidden;");
+    }
+      
+    function hideKeyboard() {
+      var keyboard = document.getElementById("keyboard");
+      keyboard.setAttribute("style", "opacity: 0; visibility: hidden;");
+      document.getElementById("buttons").setAttribute("style", "opacity: 1; visibility: visible;");
     }
